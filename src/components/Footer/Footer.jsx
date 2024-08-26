7f9da9a1189d9145b991215979368d80a3ba21d6
elbitoku <127248042+elbitoku@users.noreply.github.com>
2024-08-26 13:10:02 +0200

    modif footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return(
     <>
     <div className={styles.Footer}>
-      @ 2024 Niquesamère Entertainement. Conçu à 99,9% par Bitoky et 0,01% par l'autre Gaz. Aucun droit réservé.
+      @ 2024 Niquesamère Entertainement. Aucun droit réservé.
     </div>
     </>
   );
