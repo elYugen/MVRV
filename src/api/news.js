a276e3fbe7da082967de6963048e1117c107b79c
elbitoku <127248042+elbitoku@users.noreply.github.com>
2024-08-26 13:01:24 +0200

    Ajout d'un systeme de news
    Ajout d'une api pour gérer les news
    Ajout d'un systeme de news avec map

---
+++ src/api/news.js
@@ -0,0 +1,18 @@
+const news = [
+    // {
+    //     id: "1",
+    //     name: "Titre news",
+    //     cover: "https://r.res.easebar.com/pic/20240816/22417df2-571e-4fd8-9b68-12bdb658ac18.jpg",
+    //     resume: "Résumé de la news",
+    //     content: "Contenu de la news",
+    // },
+    // {
+    //     id: "2",
+    //     name: "Article 2",
+    //     cover: "https://leclaireur.fnac.com/wp-content/uploads/2024/03/marvel-rivals-jeu-1256x826.jpeg",
+    //     resume: "Résumé de la news",
+    //     content: "Contenu de la news",
+    // }
+];
+
+export default news;
