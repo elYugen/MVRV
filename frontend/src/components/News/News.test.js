d5f1441f916c7ad58b2f62a0838da8cd1e467fb0
elbitoku <127248042+elbitoku@users.noreply.github.com>
2024-09-02 21:35:05 +0200

    Delete frontend directory

--- frontend/src/components/News/News.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import News from './News';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<News />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- frontend/src/components/Stream/Stream.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Stream from './Stream';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Stream />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
