0e27ffaf878f47ed2ce93401baa463b71f7bd119
elbitoku <127248042+elbitoku@users.noreply.github.com>
2024-09-02 21:43:07 +0200

    Update AdminNavbar.jsx

--- frontend/src/admin/components/AdminNavbar/AdminNavbar.jsx
+++ frontend/src/admin/components/AdminNavbar/AdminNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom"; // Import useNavigate pour la redirection
+import { Link, useNavigate } from "react-router-dom";
 import styles from './AdminNavbar.module.css';
 import { FaRegUserCircle } from "react-icons/fa";
 import { SlLogout } from "react-icons/sl";
