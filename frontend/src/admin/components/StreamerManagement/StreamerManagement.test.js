f79d61ce07d36bb48b56e0c49f96c8d90e99ce21
elbitoku <127248042+elbitoku@users.noreply.github.com>
2024-09-03 09:26:58 +0200

    Delete frontend/src/admin directory

--- frontend/src/admin/components/StreamerManagement/StreamerManagement.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import StreamerManagement from './StreamerManagement';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<StreamerManagement />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
